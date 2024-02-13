e60fc78bd0e6415f9bfc27b1aa19e5bcf89e5063
UkrNicolasCage <93522793+UkrNicolasCage@users.noreply.github.com>
2024-02-13 08:10:15 +0200

    Add files via upload

---
+++ WebApplication2/WebApplication2/MyInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2
+{
+    public class MyInfo
+    {
+        public string Name { get; set; } = "";
+        public string Surname { get; set; } = "";
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; } = "";
+
+    }
+}
---
+++ WebApplication2/WebApplication2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using WebApplication2;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var companiesXml = builder.Configuration.GetSection("Companies").Get<List<CompanyInfo>>();
+var companiesJson = builder.Configuration.GetSection("Companies").Get<List<CompanyInfo>>();
+var companiesIni = builder.Configuration.GetSection("Companies").Get<List<CompanyInfo>>();
+var userConfig = builder.Configuration.GetSection("MyInformation").Get<MyInfo>();
+
+var maxNumberOfEmployees = companiesXml.Concat(companiesJson).Concat(companiesIni).OrderByDescending(c => c.NumberOfEmployees).FirstOrDefault();
+
+app.MapGet("/", () => $"Company with the highest number of employees: {maxNumberOfEmployees?.Name}\n\n" +
+$"My name and surname: {userConfig?.Name} {userConfig?.Surname}, Age: {userConfig?.Age}, Phone Number: {userConfig?.PhoneNumber}");
+app.Run();
